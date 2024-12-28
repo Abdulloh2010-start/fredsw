02c1e58a727df63282c116adc5bf98cb9d4615a0
Abdulloh2010-start <abdullohlalala@gmail.com>
2024-12-28 10:49:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("Boshlanishida Arryning uzunligi: 4")
+let result = confirm("Sizzning arrayingizdan malumotlarni olib tashlamoqchimiz");
+let a = ("Arryning uzunligi: 3")
+let fruits = ["Яблоко", "Банан", "Апельсин","Персик"];
+if (result) {
+    console.log(fruits);
+    (fruits.pop())
+    console.log(fruits);
+
+    alert(a)
+} else {
+    console.log(fruits);
+}
